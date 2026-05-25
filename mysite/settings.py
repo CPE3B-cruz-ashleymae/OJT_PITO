076f1fed8aed06807eae60fc8ced93a88143c3a8
Ashley Mae Cruz <your_correct_email@example.com>
2026-05-25 11:37:15 +0800

    Configure production static settings and deployment tools

--- mysite/settings.py
+++ mysite/settings.py
@@ -141,6 +141,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 LOGIN_URL = 'login'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
