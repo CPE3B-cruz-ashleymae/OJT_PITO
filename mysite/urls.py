4f724d63ca67895ce244f34f06cc25b0f3994401
Ashley Mae Cruz <your_correct_email@example.com>
2026-06-10 10:56:24 +0800

    Fix static file loading and base.html template

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 from django.shortcuts import redirect
 from django.contrib.auth import views as auth_views
 from users import views as user_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 # Helper function to check if the user is a superuser/admin
 def admin_required(user):
@@ -42,3 +44,6 @@ urlpatterns = [
     # Main Application
     path('', include('blog.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
